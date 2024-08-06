22d545deb166368ab69759a6d76c9371e9b4c28d
Rohit Dash <rohitdash08@gmail.com>
2024-08-06 05:33:24 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import "@/app/ui/global.css";
+import { inter } from "./ui/font";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased bg-gray-900`}>
+        {children}
+      </body>
     </html>
   );
 }
